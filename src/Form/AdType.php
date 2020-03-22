cdd48ca53c53267bb210fda3caad85ab061cf2bb
jmcestrieres <jeanmarc.cestrieres@gmail.com>
2020-03-22 02:03:55 +0100

    gestion des annonces, formulaire de création et édition

---
+++ src/Form/AdType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ad;
+use App\Form\ImageType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+class AdType extends AbstractType
+{
+    /**
+     * Permet d'avoir la configuration de base d'un champ
+     *
+     * @param string $label
+     * @param string $placeholder
+     * @param array $options
+     * @return array
+     */
+    private function getConfiguration($label, $placeholder, $options = []){
+        return array_merge([
+            'label' => $label,
+            'attr' => [
+                'placeholder' => $placeholder
+            ]
+        ], $options);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, $this->getConfiguration("Titre", "Titre de votre annonce"))
+            ->add('slug', TextType::class, $this->getConfiguration("Adresse web", "Adresse web de votre annonce (automatique)", ['required' => false]))
+            ->add('coverImage', UrlType::class, $this->getConfiguration("URL de l'image principale", "Adresse d'une image qui donne envie"))
+            ->add('introduction', TextType::class, $this->getConfiguration("Introduction", "Petite description de votre annonce"))
+            ->add('content', TextareaType::class, $this->getConfiguration("Description détaillée", "Description détaillée de votre annonce"))
+            ->add('rooms', IntegerType::class, $this->getConfiguration("Nombre de chambres", "Nombre de chambres disponibles"))
+            ->add('price', MoneyType::class, $this->getConfiguration("Prix par nuit", "Indiquez le prix demandé pour 1 nuit"))
+            ->add('images', CollectionType::class, ['entry_type' => ImageType::class, 'allow_add' => true, 'allow_delete' => true])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Ad::class,
+        ]);
+    }
+}
